1cb026c4c491c20b7e36f84433c3d189ff18ba9d
root <root@ip-172-31-32-244.ap-south-1.compute.internal>
2024-02-28 11:45:18 +0000

    comment

--- gitpullpush.sh
+++ gitpullpush.sh
@@ -1,7 +1,20 @@
 #! /bin/bash
 #read commentt
 
+#cd /home/ubuntu/myworkspace/devopsprac
+#git add . &&
+#git commit -m "latesupdate" &&
+#git push origin master
+
+echo "what is your comment_messege....?"
+
+read comment
+
 cd /home/ubuntu/myworkspace/devopsprac
-git add . &&
-git commit -m "latesupdate" &&
+
+git add .
+
+git commit -m comment
+
 git push origin master
+
